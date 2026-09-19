ce01ecd3e81e5bc941edd2fb874966eb821c6c1a
Tki_sor <1151520250@qq.com>
2026-09-19 18:11:50 +0800

    style(lang): drop same-package imports in moved ESM rewriter

--- common/src/main/java/com/tkisor/nekojs/core/module/NekoNativeEsmSourceRewriter.java
+++ common/src/main/java/com/tkisor/nekojs/core/module/NekoNativeEsmSourceRewriter.java
@@ -1,10 +1,6 @@
 package com.tkisor.nekojs.core.module;
 
 import com.tkisor.nekojs.core.compiler.NekoModuleMode;
-import com.tkisor.nekojs.core.module.NekoModuleResolver;
-import com.tkisor.nekojs.core.module.NekoModuleError;
-import com.tkisor.nekojs.core.module.NekoPreparedModule;
-import com.tkisor.nekojs.core.module.NekoResolvedModule;
 import com.tkisor.nekojs.core.module.esm.NekoEsmLinker;
 import com.tkisor.nekojs.core.module.esm.NekoEsmLinkException;
 import com.tkisor.nekojs.core.module.esm.NekoEsmDiagnostic;
@@ -17,7 +13,6 @@ import com.tkisor.nekojs.core.module.esm.NekoEsmImportDecl;
 import com.tkisor.nekojs.core.module.esm.NekoEsmExportDecl;
 import com.tkisor.nekojs.core.module.esm.NekoEsmBinding;
 import com.tkisor.nekojs.core.module.esm.NekoEsmExportKind;
-import com.tkisor.nekojs.core.module.NekoEsmVirtualModuleRegistry;
 import com.tkisor.nekojs.core.error.SourceMapRegistry;
 import com.google.gson.JsonParser;
 
